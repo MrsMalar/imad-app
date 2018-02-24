de86b7b17c1bb497e7152a938cf07d7d91cca629
MrsMalar <36625551+MrsMalar@users.noreply.github.com>
2018-02-24 01:16:58 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,16 +27,33 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
+        //create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+             // Take some action
+         if (request.status == 200) {
+            // Capture a list of names and rendor it as a list
+          var names = request.responseText;
+          names = JSON.parse(names);
+          var list ='';
+          for(var i=0; i<names.length; i++) {
+              list += '<li>' + names[i] + '</li>';
+          }
+          var ul=document.getElementById('name_list');
+          ul.innerHTML = list;
+          
+             }
+                }
+               // Not yet done
+            };
+    // Make the request
+    request.open('GET', 'http://malarvizhitamilarasan.imad.hasura-app.io/counter', true);
+    request.send(null);
   // Make a request to the server and send the name
   
-  // Capture a list of names and rendor it as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list ='';
-  for(var i=0; i<names.length; i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul=document.getElementById('name_list');
-  ul.innerHTML = list;
-  
+
 };
     
