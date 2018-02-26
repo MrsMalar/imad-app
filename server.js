39201f8bec100936defcd02e4c55fdb9dab95264
MrsMalar <36625551+MrsMalar@users.noreply.github.com>
2018-02-26 13:46:34 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,11 +126,11 @@ app.get('/articles/:articleName', function (req, res) {
     // articleName == article-one
     // articles[articleName] == content object for article one
    // SELECT * FROM article WHERE title = article-one
-   pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function(err, result){
+   pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function(err, result){
      if(err) {
          res.status(500).send(err.toString());
      }  else {
-         if(result.rows-length === 0) {
+         if(result.rows.length === 0) {
              res.status(404).send('Article Not Found');
          }else {
              var articleData = result.rows[0];
