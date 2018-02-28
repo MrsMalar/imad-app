0e6cf3a0a80d1c1ec6c8f34d166eacf2be28b823
MrsMalar <36625551+MrsMalar@users.noreply.github.com>
2018-02-28 13:51:04 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.get('/', function (req, res) {
 
 function hash(input, salt) {
     // How do we create a hash?
+    // pbkdf = Password Paste Key Derivation Function
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
