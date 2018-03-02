3a884e3073ca53dea09b81079454a9fad078f553
MrsMalar <36625551+MrsMalar@users.noreply.github.com>
2018-03-02 11:28:46 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.post('/create-user', function(req, res) {
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES($1, $2)', [username, dbString], function(req, res) {
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(req, res) {
         if(err) {
            res.status(500).send(err,toString());
        } else {
@@ -86,6 +86,7 @@ app.post('/create-user', function(req, res) {
        }
    });
 });
+
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     // Make a select request
