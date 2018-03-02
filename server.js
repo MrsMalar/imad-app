cb39954f68fbecc2b598d20a2d6c15869344da86
MrsMalar <36625551+MrsMalar@users.noreply.github.com>
2018-03-02 12:57:32 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,6 +87,31 @@ app.post('/create-user', function(req, res) {
    });
 });
 
+app.post('/login', function(req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" username = $1', [username], function(err, result) {
+        if(err) {
+           res.status(500).send(err.toString());
+       } else {
+           if (result.rows.length === 0) {
+               res.status(403).send('username/password is invalid');
+           } else {
+               // Match the password
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2]; // second element in the Hash function return array
+               var hashedPassword = hash(password, salt);
+               if(hashedPassword === dbString) {
+                   res.send('Credentials correct !');
+               } else {
+                   res.status(403).send('username/password is invalid');
+               }
+           }
+           res.send('User Successfully created: ' + username);
+       }
+   });
+});
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     // Make a select request
