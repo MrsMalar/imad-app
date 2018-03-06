e260f7b14640ee24e41532e40453e703abf4d4ca
MrsMalar <36625551+MrsMalar@users.noreply.github.com>
2018-03-06 11:18:06 +0100

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -19,7 +19,7 @@ function loadCommentForm () {
         
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
-          if (request.readystate === XMLHttpRequest.DONE) {
+          if (request.readyState === XMLHttpRequest.DONE) {
                 // Take some action
                 if (request.status === 200) {
                     // clear the form & reload all the comments
