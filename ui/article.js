3cc0b480dc7b72a03ac5fba31774100fceb91d5e
MrsMalar <36625551+MrsMalar@users.noreply.github.com>
2018-03-06 12:39:17 +0100

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -35,12 +35,13 @@ function loadCommentForm () {
           }
         };
         
-        // Make the request
+       // Make the request
         var comment = document.getElementById('comment_text').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({"comment": comment})); 
+        request.send(JSON.stringify({comment: comment}));  
         submit.value = 'Submitting...';
+         
         
     };
 }
